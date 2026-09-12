68eb4ac7f42dace3e79f334eaf9f6908d7cdd998
Ahnaf Shahriar Abir <abirahnafshahriar@gmail.com>
2026-09-13 01:45:32 +0600

    navbar sticky fix

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -3,28 +3,39 @@ import logo from "../../assets/logo-text.png";
 
 const Navbar = () => {
   return (
-    <>
-      <nav className="border-b border-gray-200 sticky top-0 z-50 bg-white/80 backdrop-blur-md border-b border-gray-100 transition-all">
-        <div className="flex justify-between container mx-auto px-6 py-4">
-          <img src={logo} alt="" className="h-10" />
+    <nav className="sticky top-0 z-50 bg-white/80 backdrop-blur-md border-b border-gray-100 transition-all">
+      <div className="container mx-auto px-6 py-4 flex items-center justify-between">
+        {/* Logo */}
+        <img src={logo} alt="Dev Stack Logo" className="h-10" />
 
-          <ul className="flex items-center gap-4 cursor-pointer">
-            <li className="text-[#DB2777]">Home</li>
-            <li className="text-[#475569]">Technologies</li>
-            <li className="text-[#475569]">Projects</li>
-            <li className="text-[#475569]">About</li>
-            <li className="text-[#475569]">Contact</li>
-          </ul>
+        {/* Links */}
+        <ul className="flex items-center gap-6 cursor-pointer font-medium">
+          <li className="text-[#DB2777]">Home</li>
+          <li className="text-[#475569] hover:text-[#DB2777] transition">
+            Technologies
+          </li>
+          <li className="text-[#475569] hover:text-[#DB2777] transition">
+            Projects
+          </li>
+          <li className="text-[#475569] hover:text-[#DB2777] transition">
+            About
+          </li>
+          <li className="text-[#475569] hover:text-[#DB2777] transition">
+            Contact
+          </li>
+        </ul>
 
-          <div className="flex gap-4">
-            <button className="cursor-pointer tect-[#334155]">Sign In</button>
-            <button className="cursor-pointer bg-[#d91b7e] text-[#FFFFFF] px-8 py-3 rounded-full">
-              Sign Up
-            </button>
-          </div>
+        {/* Buttons */}
+        <div className="flex items-center gap-4">
+          <button className="cursor-pointer text-[#334155] hover:text-black font-medium text-sm">
+            Sign In
+          </button>
+          <button className="cursor-pointer bg-[#DB2777] hover:bg-[#be185d] text-white px-6 py-2.5 rounded-full font-medium text-sm transition shadow-sm">
+            Sign Up
+          </button>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 };
 
